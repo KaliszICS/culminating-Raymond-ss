66f1888ef871c4c6832755b71773abecb28ed4f1
Raymond-ss <343383188@tdsb.ca>
2026-06-02 15:39:32 +0000

    06/02/2026

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,34 +1,37 @@
+/* Steps and instruction checks in correct order? 
+	Declearing 
+	Create cardeck and how to draw the cards from the deck
+	Players cards - their five handheld cards
+	Poker Hands - tier ranking strongest to weakest hands
+
+	Ante; Player bet phase 1: all chips be amount must be the same draw all 5 cards
+	after drawing all 5 cards - players must bet to continue the play
+	After those player have bet; thier allowed to discard up to 3 cards and draw new draw(depends on discard cards)
+	After the Draw the player are allowed to bet once again; 
+	After the bet phase is over those player that bet flip their cards over to see who has strongest hand. 
+ */
+
+import javax.smartcardio.Card;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+	String suit[] = {"Diamonds","Hearts", "Clubs", "Spades"};
+	String rank[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
+	String deck[] = new String[52];
+	//Assigning values to each card in the deck
+	int ctr = 0;
 
+	for (int i = 0; ctr < 4; ++i) {
+		for (int j = 0; j < 13; ++j) {
+			deck[ctr] = deck(rank[j], suit[i]);
+			++ctr;
+		}
 	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	System.out.println(deck);
 	}
 
 }
 
-L
-=
- cardDeck ==-\]
-/
 
 
